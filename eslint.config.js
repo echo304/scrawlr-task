b995f8bf0d875a2ab89d60c96d541af66de3bea7
leesb <echo3042@gmail.com>
2024-08-28 21:38:40 -0700

    Update configs for alias and testing

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,8 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
-import pluginReact, { rules } from "eslint-plugin-react";
+import pluginReact from "eslint-plugin-react";
+import airbnb from "eslint-config-airbnb";
 
 
 export default [
@@ -10,9 +11,9 @@ export default [
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   pluginReact.configs.flat.recommended,
+  airbnb,
   {
     rules: {
-      ...rules,
       "react/react-in-jsx-scope": "off",
     },
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,22 @@
+/// <reference types="vitest" />
+import * as path from "path";
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@context": path.resolve(__dirname, "./src/context"),
+      "@lib": path.resolve(__dirname, "./src/lib"),
+    }
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.ts'],
+  },
 })
